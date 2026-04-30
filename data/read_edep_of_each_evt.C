91f720d4c8d8cc8dfbd8bb4303eb9839a7c49cf3
wzc <2292250560@qq.com>
2026-04-30 09:40:02 +0800

    Update read_edep_of_each_ent.C

--- data/read_edep_of_each_evt.C
+++ data/read_edep_of_each_evt.C
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <vector>
+#include <utility>
+#include <map>
 #include "ROOT/RDataFrame.hxx"
 #include "TTree.h"
 #include "TFile.h"
 
 int read_edep_of_each_evt(){
-    ROOT::EnableImplicitMT(18);
-    auto file{TFile::Open("edep_of_each_evt.root","RECREATE")};
+    ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("G4Run0/ECALSimHit","SimMACEPhaseI_20260401.root");
-    short nMod{ static_cast<short>(*df.Max("ModID")+1) };
+    auto nMod{ static_cast<short>(*df.Max("ModID")+1) };
+    auto nEvt{static_cast<int>(*df.Max("EvtID")+1)};
 
-    int nEvt{static_cast<int>(*df.Max("EvtID")+1)};
-    short modID;
-    float edep;
+    std::map<std::pair<int, short>, float> evt_mod_edep_map;
+    df.Foreach([&](int evtID, short modID, float edep){
+        evt_mod_edep_map[{evtID, modID}] += edep;
+    }, {"EvtID","ModID","Edep"});
+
+    auto file{TFile::Open("edep_of_each_evt.root","RECREATE")};
+    auto tree = new TTree("EdepOfEachEvt", "Edep of each event");
     std::vector<float> edeps(nMod);
-
-    TTree* tree = new TTree("EdepOfEachEvt", "Edep of each event");
     tree->Branch("Edeps", &edeps);
 
-    int evtID{ 0 };
-    while (evtID < nEvt){
-        printf("Grabing edeps of crystals of event%d\n", evtID);
-        modID = 0;
-        auto evt_df = df.Filter([&](int x){return x==evtID;},{"EvtID"});
-        while (modID < nMod){
-            edep = *(evt_df.Filter([&](short x){return x==modID;},{"ModID"})
-                           .Sum<float>("Edep"));
-            edeps.emplace_back(edep);
-            ++modID;
+    for (int evt{0}; evt < nEvt; ++evt){
+        printf("Writing vector edeps of event%d\n", evt);
+        for (short mod{0}; mod < nMod; ++mod){
+            auto it = evt_mod_edep_map.find({evt, mod});
+            if (it == evt_mod_edep_map.end()){
+                printf("[Abnormal]: event%d, mod%d edep not found in map\n", evt, mod);
+                edeps[mod] = 0.0f;
+                continue;
+            }
+            edeps[mod] = it->second;
         }
         tree->Fill();
-        ++evtID;
         edeps.clear();
     }
+
     file->Write();
     file->Close();
 
